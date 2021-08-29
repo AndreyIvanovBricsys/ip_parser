f351e18c07292ee23783d0c850abaee580b7340f
Andrey Ivanov <andrey.ivanov@hexagon.com>
2021-08-30 00:00:20 +0700

    Implemented missing functionality

--- main.cpp
+++ main.cpp
@@ -3,6 +3,76 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <array>
+#include <stdint.h>
+#include <algorithm>
+#include <fstream>
+#include <functional>
+
+class IPv4Address
+{
+public:
+	IPv4Address(const std::vector<std::string> & stringValues)
+	{
+		if (stringValues.size() != 4)
+		{
+			throw std::invalid_argument{ "" };
+		}
+
+		std::transform(stringValues.cbegin(), stringValues.cend(), values.begin(),
+			[](const auto & stringValue)
+			{
+				return static_cast<std::uint8_t>(std::stoi(stringValue));
+			});
+	}
+
+	uint8_t getValueAt(size_t index) const
+	{
+		return values.at(index);
+	}
+
+	const std::array<uint8_t, 4> & getValues() const
+	{
+		return values;
+	}
+
+	bool operator<(const IPv4Address & other) const
+	{
+		for (decltype(values)::size_type index{ 0 }; index < values.size(); ++index)
+		{
+			if (values[index] < other.values[index])
+			{
+				return true;
+			}
+			else if (values[index] > other.values[index])
+			{
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	bool operator>(const IPv4Address & other) const
+	{
+		return other < *this;
+	}
+
+private:
+	std::array<uint8_t, 4> values;
+
+	friend std::ostream & operator<<(std::ostream &, const IPv4Address &);
+};
+
+std::ostream & operator<<(std::ostream & out, const IPv4Address & address)
+{
+	auto & ipv4Values{ address.values };
+	return out <<
+		static_cast<int>(ipv4Values[0]) << '.' <<
+		static_cast<int>(ipv4Values[1]) << '.' <<
+		static_cast<int>(ipv4Values[2]) << '.' <<
+		static_cast<int>(ipv4Values[3]);
+}
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -10,52 +80,66 @@
 // ("11.", '.') -> ["11", ""]
 // (".11", '.') -> ["", "11"]
 // ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string & str, char d)
+std::vector<std::string> split(const std::string & str, char delimiter)
 {
-	std::vector<std::string> r;
+	std::vector<std::string> result;
 
 	std::string::size_type start = 0;
-	std::string::size_type stop = str.find_first_of(d);
+	std::string::size_type stop = str.find_first_of(delimiter);
 	while (stop != std::string::npos)
 	{
-		r.push_back(str.substr(start, stop - start));
+		result.push_back(str.substr(start, stop - start));
 
 		start = stop + 1;
-		stop = str.find_first_of(d, start);
+		stop = str.find_first_of(delimiter, start);
 	}
 
-	r.push_back(str.substr(start));
+	result.push_back(str.substr(start));
 
-	return r;
+	return result;
+}
+
+void processIPs(const std::vector<IPv4Address> & ipList,
+	std::function<bool(const IPv4Address &)> accept,
+	std::function<void(const IPv4Address &)> process)
+{
+	for (const auto & ip : ipList)
+	{
+		if (accept(ip))
+		{
+			process(ip);
+		}
+	}
 }
 
 int main(int argc, char const * argv[])
 {
 	try
 	{
-		std::vector<std::vector<std::string> > ip_pool;
+		std::string inputFilePath;
+		std::cin >> inputFilePath;
+		std::ifstream inputFile{ inputFilePath };
 
-		for (std::string line; std::getline(std::cin, line);)
+		std::string outputFilePath;
+		std::cin >> outputFilePath;
+		std::ofstream outputFile{ outputFilePath };
+
+		std::vector<IPv4Address> ip_pool;
+
+		for (std::string line; std::getline(inputFile, line);)
 		{
-			std::vector<std::string> v = split(line, '\t');
-			ip_pool.push_back(split(v.at(0), '.'));
+			auto tokens{ split(line, '\t') };
+			ip_pool.emplace_back(split(tokens.at(0), '.'));
 		}
 
-		// TODO reverse lexicographically sort
+		std::sort(ip_pool.begin(), ip_pool.end(), std::greater<IPv4Address>());
 
-		for (std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+		auto printIP = [&outputFile](const IPv4Address & ip)
 		{
-			for (std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
-			{
-				if (ip_part != ip->cbegin())
-				{
-					std::cout << ".";
+			outputFile << ip << std::endl;
+		};
 
-				}
-				std::cout << *ip_part;
-			}
-			std::cout << std::endl;
-		}
+		processIPs(ip_pool, [](const IPv4Address &) { return true; }, printIP);
 
 		// 222.173.235.246
 		// 222.130.177.64
@@ -65,8 +149,12 @@ int main(int argc, char const * argv[])
 		// 1.29.168.152
 		// 1.1.234.8
 
-		// TODO filter by first byte and output
-		// ip = filter(1)
+		processIPs(ip_pool,
+			[](const IPv4Address & ip)
+			{
+				return ip.getValueAt(0) == 1;
+			},
+			printIP);
 
 		// 1.231.69.33
 		// 1.87.203.225
@@ -74,16 +162,25 @@ int main(int argc, char const * argv[])
 		// 1.29.168.152
 		// 1.1.234.8
 
-		// TODO filter by first and second bytes and output
-		// ip = filter(46, 70)
+		processIPs(ip_pool,
+			[](const IPv4Address & ip)
+			{
+				return  (ip.getValueAt(0) == 46) && (ip.getValueAt(1) == 70);
+			},
+			printIP);
 
 		// 46.70.225.39
 		// 46.70.147.26
 		// 46.70.113.73
 		// 46.70.29.76
 
-		// TODO filter by any byte and output
-		// ip = filter_any(46)
+		processIPs(ip_pool,
+			[](const IPv4Address & ip)
+			{
+				auto & values{ ip.getValues() };
+				return std::any_of(values.cbegin(), values.cend(), [](auto val) { return val == 46; });
+			},
+			printIP);
 
 		// 186.204.34.46
 		// 186.46.222.194
